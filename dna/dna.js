d7ff6188bc4c9584253ef3daf339cede79362f06
Jonas Almeida <jalmeida@mathbiol.org>
2019-10-07 15:01:19 -0400

    dna

---
+++ dna/dna.js
@@ -0,0 +1,12 @@
+console.log('dna.js loaded');
+
+(function(){ // going anonymous for the sake of packaging
+    const dna = function(){
+        console.log('dna.js initiatized at '+Date())
+        //ini
+    }
+    //
+    if(typeof(window)=="object"){
+        window.dna=dna // drop it in the global scope 
+    }
+})()
--- dna/usm2.js
+++
@@ -1,1 +0,0 @@
-console.log('usm2.js loaded')
