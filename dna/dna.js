bf10e56fabead6944fd4254535eb0729b4235c14
Jonas Almeida <jalmeida@mathbiol.org>
2019-10-07 15:07:30 -0400

    dna

--- dna/dna.js
+++ dna/dna.js
@@ -3,6 +3,7 @@ console.log('dna.js loaded');
 (function(){ // going anonymous for the sake of packaging
     const dna = function(){
         console.log('dna.js initiatized at '+Date())
+        this.date=Date()
         //ini
     }
     //
